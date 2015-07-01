87e879ab78b31fc98e1731e3b5506623b3989f2e
astrofelipe <felipe.irh@gmail.com>
2015-07-01 16:43:55 -0300

    Multiprocess
    Mitad de los CPU + 1

--- 1a1/match_1a1.py
+++ 1a1/match_1a1.py
@@ -1,8 +1,7 @@
 import os
 import glob
 import sys
-from astropy.utils.console import ProgressBar
-
+import multiprocessing as mp 
 ######### INPUT USUARIO
 #python match1a1.py folder ref_cat
 folder    = sys.argv[1] #path al directorio de las imagenes/catalogos
@@ -20,4 +19,9 @@ def match(cat):
     #os.system('java -jar -Xmx4096M stilts.jar tmatch2 in1='+ref_cat+' values1="RA DEC" ifmt1=ascii in2='+cat+' values2="RA DEC" ifmt2=ascii matcher=sky params="0.3" find=best join=1and2 out='+cat.replace('.dat','_'+cat[-7:-4]+'.match')+' ofmt=ascii')
     os.system('java -jar stilts.jar tmatch2 in1='+ref_cat+' values1="RA DEC" ifmt1=ascii in2='+cat+' values2="RA DEC" ifmt2=ascii matcher=sky params="0.3" find=best join=1and2 out='+cat.replace('.dat','_'+cat[-7:-4]+'.match')+' ofmt=ascii')
 
-ProgressBar.map(match,catalog,multiprocess=True)
+cpus = (1 + mp.cpu_count()/2)
+
+pool = mp.Pool(processes=cpus)
+pool.map(match,catalog)
+
+#ProgressBar.map(match,catalog,multiprocess=True)
