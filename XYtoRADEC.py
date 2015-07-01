8f18a0100eb9797d814a385edc9ae99a8aa9c135
astrofelipe <felipe.irh@gmail.com>
2015-07-01 14:43:27 -0300

    Final (?)

--- XYtoRADEC.py
+++ XYtoRADEC.py
@@ -1,6 +1,6 @@
 import numpy as np
 from astropy import wcs
-from astropy.io import fits
+from astropy.io import fits as pf
 from astropy import wcs
 from astropy.utils.console import ProgressBar, color_print
 import os
@@ -16,19 +16,18 @@ def XYtoRADEC(ep):
 	id,x,y,mag = np.loadtxt(cfn,usecols=range(4),skiprows=3,unpack=True)
 	id = id.astype(int)
 	
-	hdr    = fits.open(ffn)[0].header
+	hdr    = pf.open(ffn)[0].header
 	w      = wcs.WCS(hdr)
 	ra,dec = np.transpose(w.wcs_pix2world(np.transpose([x,y]),1))
 	
 	head = 'ID RA DEC X Y MAG'
 	fmt  = '%d %.7f %.7f %.3f %.3f %.3f'
-	np.savetxt(folder+ffn.replace('fits','dat'),np.transpose([id,ra,dec,x,y,mag]),header=head,fmt=fmt)
+	np.savetxt(ffn.replace('fits','dat'),np.transpose([id,ra,dec,x,y,mag]),header=head,fmt=fmt)
 
 archivos  = np.sort([f for f in os.listdir('./')])
-fit	  = np.sort([f for f in archivos if f.endswith('.fits')])
+fits	  = np.sort([f for f in archivos if f.endswith('.fits')])
 catalog	  = np.sort([f for f in archivos if f.endswith('.dao')])
 
 color_print('-Obteniendo RADEC...','cyan')
-ProgressBar.map(XYtoRADEC,np.transpose([fits,catalog]),multiprocess=True)
 ProgressBar.map(XYtoRADEC,np.transpose([fits,catalog]),multiprocess=True)
 
