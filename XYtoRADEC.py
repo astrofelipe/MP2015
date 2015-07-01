e962d717386e81064def545b87ba6019b2386384
astrofelipe <astrofelipe@users.noreply.github.com>
2015-07-01 14:29:26 -0300

    Update XYtoRADEC.py

--- XYtoRADEC.py
+++ XYtoRADEC.py
@@ -12,13 +12,22 @@ os.chdir('path')
 def XYtoRADEC(ep):
 	ffn,cfn = ep
 
-	id,x,y,mag = np.loadtxt(folder+cfn,usecols=range(4),skiprows=3,unpack=True)
+	id,x,y,mag = np.loadtxt(cfn,usecols=range(4),skiprows=3,unpack=True)
 	id = id.astype(int)
 	
-	hdr    = fits.open(folder+ffn)[0].header
+	hdr    = fits.open(ffn)[0].header
 	w      = wcs.WCS(hdr)
 	ra,dec = np.transpose(w.wcs_pix2world(np.transpose([x,y]),1))
 	
 	head = 'ID RA DEC X Y MAG'
 	fmt  = '%d %.7f %.7f %.3f %.3f %.3f'
 	np.savetxt(folder+ffn.replace('fits','dat'),np.transpose([id,ra,dec,x,y,mag]),header=head,fmt=fmt)
+
+archivos  = np.sort([f for f in os.listdir('./')])
+fit	  = np.sort([f for f in archivos if f.endswith('.fits')])
+catalog	  = np.sort([f for f in archivos if f.endswith('.dao')])
+
+color_print('-Obteniendo RADEC...','cyan')
+ProgressBar.map(XYtoRADEC,np.transpose([fits,catalog]),multiprocess=True)
+ProgressBar.map(XYtoRADEC,np.transpose([fits,catalog]),multiprocess=True)
+
