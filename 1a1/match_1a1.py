6edcc20e941ae6ff59503f5d6d6bae02f28e6dd6
astrofelipe <astrofelipe@users.noreply.github.com>
2015-07-01 16:23:42 -0300

    Update match_1a1.py

--- 1a1/match_1a1.py
+++ 1a1/match_1a1.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import sys
+from astropy.utils.console import ProgressBar
 
 ######### INPUT USUARIO
 #python match1a1.py folder ref_cat
@@ -10,10 +11,13 @@ ref_cat   = folder+sys.argv[2] #catalogo de referencia
 
 catalog  = sorted(glob.glob(folder+'*k*.dat'))
 
-for cat in catalog:
+#for cat in catalog:
+def match(cat):
 
     if cat == ref_cat:
         continue
 
     #os.system('java -jar -Xmx4096M stilts.jar tmatch2 in1='+ref_cat+' values1="RA DEC" ifmt1=ascii in2='+cat+' values2="RA DEC" ifmt2=ascii matcher=sky params="0.3" find=best join=1and2 out='+cat.replace('.dat','_'+cat[-7:-4]+'.match')+' ofmt=ascii')
     os.system('java -jar stilts.jar tmatch2 in1='+ref_cat+' values1="RA DEC" ifmt1=ascii in2='+cat+' values2="RA DEC" ifmt2=ascii matcher=sky params="0.3" find=best join=1and2 out='+cat.replace('.dat','_'+cat[-7:-4]+'.match')+' ofmt=ascii')
+
+ProgressBar.map(match,catalog,multiprocess=True)
