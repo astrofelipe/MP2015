6cb40620281883f6e1220460b8f2678e2882f014
fegran <pipegran@gmail.com>
2015-07-03 11:23:51 -0400

    FG
    Cambio de extension

--- 1a1/match_1a1.py
+++ 1a1/match_1a1.py
@@ -1,7 +1,7 @@
 import os
 import glob
 import sys
-import multiprocessing as mp 
+import multiprocessing as mp
 ######### INPUT USUARIO
 #python match1a1.py folder ref_cat
 folder    = sys.argv[1] #path al directorio de las imagenes/catalogos
@@ -17,7 +17,7 @@ def match(cat):
         return
 
     #os.system('java -jar -Xmx4096M stilts.jar tmatch2 in1='+ref_cat+' values1="RA DEC" ifmt1=ascii in2='+cat+' values2="RA DEC" ifmt2=ascii matcher=sky params="0.3" find=best join=1and2 out='+cat.replace('.dat','_'+cat[-7:-4]+'.match')+' ofmt=ascii')
-    os.system('java -jar stilts.jar tmatch2 in1='+ref_cat+' values1="RA DEC" ifmt1=ascii in2='+cat+' values2="RA DEC" ifmt2=ascii matcher=sky params="0.3" find=best join=1and2 out='+cat.replace('.dat','_'+ref_cat[-7:-4]+'.match')+' ofmt=ascii')
+    os.system('java -jar stilts.jar tmatch2 in1='+ref_cat+' values1="RA DEC" ifmt1=ascii in2='+cat+' values2="RA DEC" ifmt2=ascii matcher=sky params="0.3" find=best join=1and2 out='+cat.replace('.dat','_'+ref_cat[-7:-4]+'.mat')+' ofmt=ascii')
 
 cpus = (1 + mp.cpu_count()/2)
 
