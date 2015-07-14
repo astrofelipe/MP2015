a2073ee02254589c04e89a4837e37a11dc702b42
astrofelipe <felipe.irh@gmail.com>
2015-07-14 15:48:59 -0300

    FR
    Crea un CMD en base a una época K y una J a elección

---
+++ CMD.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import glob
+import numpy as np
+
+#Numero de epocas a usar
+k_epoch = int(sys.argv[1])
+j_epoch = int(sys.argv[2])
+
+output = 'CMD_%03d-%03d.dat' % (k_epoch, j_epoch)
+
+#Busca los catalogos
+k_catalog = glob.glob('*k*%03d.dat' % k_epoch)[0]
+j_catalog = glob.glob('*j*%03d.dat' % j_epoch)[0]
+
+#Realiza el match usando K como referencia (1 and 2)
+print 'Archivos para generar el CMD: %s %s\n' % (k_catalog, j_catalog)
+os.system('java -jar stilts.jar tmatch2 \
+           in1=%s values1="RA DEC" ifmt1=ascii \
+           in2=%s values2="RA DEC" ifmt2=ascii \
+           matcher=sky params=0.3 find=best join=1and2 \
+           out=%s ofmt=ascii' % (k_catalog,j_catalog,output) )
+
+if sys.argv[3] == '-p':
+    import matplotlib.pyplot as plt
+    K,J = np.genfromtxt(output,unpack=True,usecols=(5,12))
+
+    fig, ax = plt.subplots()
+    ax.plot(J-K,K,'.k',ms=2,alpha=.5)
+    ax.set_xlabel('$J-K$')
+    ax.set_ylabel('$K$')
+    ax.invert_yaxis()
+    plt.show()
