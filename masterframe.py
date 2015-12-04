3cede7f6d62fe65a6695c807c85ad7dbf010ca43
astrofelipe <felipe.irh@gmail.com>
2015-12-04 20:13:15 -0300

    4 dic

---
+++ masterframe.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sys
+import os
+
+match_tol     = .3
+stilts_folder = os.path.dirname(os.path.realpath(__file__)) #STILTS debe estar con el archivo .py
+
+#Lee el archivo con los Inputs
+inputs = sys.argv[1]
+archivos = np.genfromtxt(inputs, dtype='string')
+
+supermatch  = 'java -jar %s/stilts.jar tmatchn multimode=group nin=%d matcher=sky params=%.3f ' % (stilts_folder, len(archivos), match_tol)
+for i in range(1, len(archivos)+1):
+    supermatch += 'in%d=%s ifmt%d=ascii values%d="RA DEC" join%d=always ' % (i, archivos[i-1], i, i, i)
+supermatch += 'out=supermatch.dat ofmt=ascii'
+
+os.system(supermatch)
