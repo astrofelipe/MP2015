76b53f27e0791c9ff3b78d4c684308f190fcd09b
astrofelipe <felipe.irh@gmail.com>
2015-12-22 10:13:08 -0300

    Seleccionar columna

--- 1a1/master_stilts.py
+++ 1a1/master_stilts.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 match_tol     = .3
+modo          = 'RA DEC'                                     #Columnas para hacer el match (ej 'RA DEC' o 'ID')
 stilts_folder = os.path.dirname(os.path.realpath(__file__)) #STILTS debe estar con el archivo .py
 
 #Lee el archivo con los Inputs
@@ -11,7 +12,7 @@ archivos = np.genfromtxt(inputs, dtype='string')
 
 supermatch  = 'java -jar %s/stilts.jar tmatchn multimode=group nin=%d matcher=sky params=%.3f ' % (stilts_folder, len(archivos), match_tol)
 for i in range(1, len(archivos)+1):
-    supermatch += 'in%d=%s ifmt%d=ascii values%d="RA DEC" join%d=always ' % (i, archivos[i-1], i, i, i)
+    supermatch += 'in%d=%s ifmt%d=ascii values%d=%s join%d=always ' % (i, archivos[i-1], i, modo, i, i)
 supermatch += 'out=master_stilts.dat ofmt=csv'
 
 print supermatch
