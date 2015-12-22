2d20f50d720bb35ddf2cc0a054fe28ac347f7611
astrofelipe <felipe.irh@gmail.com>
2015-12-22 15:31:57 -0300

    Modos

--- 1a1/master_stilts.py
+++ 1a1/master_stilts.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import os
 
-match_tol     = .3
+match_tol     = .3                                          #Tolerancia para el modo a usar (ID no usa este parametro)
 modo          = 'RA DEC'                                     #Columnas para hacer el match (ej 'RA DEC' o 'ID')
 stilts_folder = os.path.dirname(os.path.realpath(__file__)) #STILTS debe estar con el archivo .py
 
@@ -10,7 +10,12 @@ stilts_folder = os.path.dirname(os.path.realpath(__file__)) #STILTS debe estar c
 inputs = sys.argv[1]
 archivos = np.genfromtxt(inputs, dtype='string')
 
-supermatch  = 'java -jar %s/stilts.jar tmatchn multimode=group nin=%d matcher=sky params=%.3f ' % (stilts_folder, len(archivos), match_tol)
+#Selecciona modo
+matcher = 'sky params=%.3f' % match_tol
+if modo == 'ID':
+    matcher = 'exact'
+
+supermatch  = 'java -jar %s/stilts.jar tmatchn multimode=group nin=%d matcher=%s ' % (stilts_folder, len(archivos), matcher)
 for i in range(1, len(archivos)+1):
     supermatch += 'in%d=%s ifmt%d=ascii values%d=%s join%d=always ' % (i, archivos[i-1], i, modo, i, i)
 supermatch += 'out=master_stilts.dat ofmt=csv'
