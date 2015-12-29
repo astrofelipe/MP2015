c44bc632f13d5c285210282ce445340c3bd35c6c
astrofelipe <felipe.irh@gmail.com>
2015-12-29 14:25:13 -0300

    Orden

--- 1a1/master_stilts.py
+++ 1a1/master_stilts.py
@@ -4,9 +4,10 @@ import os
 
 #PARAMETROS A EDITAR
 match_tol     = .3                                          #Tolerancia para el modo a usar (ID no usa este parametro)
-modo          = 'RA DEC'                                     #Columnas para hacer el match (ej 'RA DEC' o 'ID')
+#modo          = 'RA DEC'                                     #Columnas para hacer el match (ej 'RA DEC' o 'ID')
+modo          = 'ID'                                     #Columnas para hacer el match (ej 'RA DEC' o 'ID')
 
-#FIN
+#FIN PARAMETROS
 stilts_folder = os.path.dirname(os.path.realpath(__file__)) #STILTS debe estar con el archivo .py
 
 #Lee el archivo con los Inputs
