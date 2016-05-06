6481666219876c381d48a66c90a9763e1a7c637d
astrofelipe <felipe.irh@gmail.com>
2016-05-05 22:21:51 -0300

    Print

--- refstars.py
+++ refstars.py
@@ -10,23 +10,29 @@ from astropy.utils.console import color_print
 color_print('REFSTARS.py', 'yellow')
 
 if len(sys.argv) == 1:
-    color_print()
+    print('Para usar:')
+    color_print('\tpython refstars.py ','cyan','<archivo con CMD> <archivo con puntos xy del poligono>','lightcyan')
+    sys.exit(1)
 
 #Argumentos
 cmd_file = sys.argv[1]  #CMD (.mat o .matgc)
 ver_file = sys.argv[2]  #Vertices del poligono
 
+print('\tLeyendo input...')
 data  = np.genfromtxt(cmd_file)
 K     = data[:,5]
 J_K   = data[:,12] - K
 verts = np.genfromtxt(ver_file)
 
+print('\tBuscando puntos dentro del poligono...')
 path = Path(verts)
 idx  = path.contains_points(np.transpose([J_K,K]))
+print('\t\t %d de %d estrellas seleccionadas' % (idx.sum(), len(data)))
 
 refstars = data[idx]
 hdr = 'ID RA_1 DEC_1 X_1 Y_1 MAG_1 MAG_ERR_1 ID_2 RA_2 DEC_2 X_2 Y_2 MAG_2 MAG_ERR_2'
 fmt = '%d %.7f %.7f %.3f %.3f %.3f %.3f %d %.7f %.7f %.3f %.3f %.3f %.3f'
+print('\tGuardando...')
 np.savetxt('arefstars.dat', refstars, header=hdr, fmt=fmt)
 
 '''
