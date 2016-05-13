2b7e5e82f6ed558bc17ab3be1843c3810559ac5f
astrofelipe <felipe.irh@gmail.com>
2016-05-13 01:59:05 -0300

    Aaaah

--- pm_1a1.py
+++ pm_1a1.py
@@ -134,7 +134,7 @@ see   = see[yr_ma]
 #Quiero solo las epocas con las que estoy trabajando
 #Busca si el numero de epoca esta en yr_ma
 molde = yep[0].split('-')[0]
-tengo = np.array([molde+'%03d.fits' % i for i in nro_epoca])
+tengo = np.array([molde+'-%03d.fits' % i for i in nro_epoca])
 
 print yep
 print tengo
