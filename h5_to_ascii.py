370e7579fb37c6889fea727e0607c0ff772823bc
astrofelipe <felipe.irh@gmail.com>
2016-05-20 16:32:07 -0300

    h5

--- h5_to_ascii.py
+++ h5_to_ascii.py
@@ -1,5 +1,6 @@
 from astropy.table import Table
 import sys
+import subprocess
 
 #Input y output
 inf  = sys.argv[1]
@@ -8,5 +9,5 @@ outf = sys.argv[1].replace('h5', 'dat')
 #Abre el h5 como tabla
 print '\nAbriendo archivo...'
 t = Table.read(inf, path='data')
-print '\nGuardando como ASCII...'
-t.write(outf, format='ascii.daophot')
+print '\nGuardando como ASCII (CSV)...'
+subprocess.call("sed -i 's/nan//g' %s" % output, shell=True)
