5576597f7ec743ca39fff5409ba289a96ad5f0d5
astrofelipe <felipe.irh@gmail.com>
2016-05-23 15:37:50 -0300

    ID

--- extract.py
+++ extract.py
@@ -5,4 +5,8 @@ theid = sys.argv[1]
 
 with h5py.File('PM.h5') as f:
     ids = f['data'][:,0]
-    print ids
+    idx = ids == theid
+
+    if idx.sum() == 0:
+        print 'ID no encontrado!'
+        sys.exit(1)
