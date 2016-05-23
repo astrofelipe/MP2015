7c9d17e7664d82510aefa829eb4d8cd5f331f4e8
astrofelipe <felipe.irh@gmail.com>
2016-05-23 16:17:27 -0300

    verbose

--- extract.py
+++ extract.py
@@ -7,7 +7,9 @@ import numpy as np
 theid = int(sys.argv[1])
 hidenan = True
 
+print('Abriendo archivo...')
 with h5py.File('PM.h5') as f:
+    print('Buscando ID')
     ids = f['data'][:,0]
     idx = ids == theid
 
@@ -15,9 +17,11 @@ with h5py.File('PM.h5') as f:
         print('ID no encontrado!')
         sys.exit(1)
 
+    print('Buscando archivos de epocas')
     pms = glob.glob('PMs/*.dat')
     pms = np.sort([int(s.split('_')[-1].split('.')[0]) for s in pms])
 
+    print('Extrayendo DX y DY')
     idx = np.where(idx)[0][0]
     row = f['data'][idx]
     dx  = row[8::4]
@@ -25,7 +29,8 @@ with h5py.File('PM.h5') as f:
 
     output = np.vstack((pms, dx, dy)).T
     if hidenan:
+        print('No se guardaran filas donde la estrella no esta')
         output = output[np.isfinite(dx)]
 
-
+    print('Guardando...')
     np.savetxt('id_estrella_de_interes.dat', output, fmt='%03d %.3f %.3f', header='EPOCH DX DY')
